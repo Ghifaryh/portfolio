95eaf4bbd62eacfbd003a146bacff81f0c065bf4
Ghifary <mhghifaryy@gmail.com>
2026-01-26 14:30:37 +0700

    why its broken

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react"
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/", // Changed from "/portfolio/" to "/"
+  base: "/portfolio/", // Changed from "/portfolio/" to "/"
 })
